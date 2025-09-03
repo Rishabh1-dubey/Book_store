8fe0121714c99a1bbeab6550bc9325eb4f536f0c
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-09-03 18:28:24 +0530

    getting started with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className='bg-black'>this is all about beggniing of the project</div>
+    <>
+    <nav className='font-secondary'>Navbarr</nav>
+    <Outlet/>
+    <footer>Foooter senction</footer>
+    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const approuter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+       path:"/",
+       element:<h1>App</h1>
+      },
+      {
+        path:"/about",
+        element:<h2>the element is about</h2>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={approuter} />
+  </React.StrictMode>
+);
