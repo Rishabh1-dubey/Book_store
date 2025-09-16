a4a4e4e633c3d9ee29e43e1a491c299a47edca3f
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-09-16 18:17:33 +0530

    on the way to complete frontend logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
-
-import './App.css'
-import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './pages/home/Footer'
-
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./pages/home/Footer";
+import { Provider } from "react-redux";
+import appStore from "./redux/store/appStore";
+import { Toaster } from "sonner";
 function App() {
-
-
   return (
-    <div className='max-w-screen-xl mx-auto'>
-
-   
-   <Navbar/>
-    <main className='min-h-screen mx-auto  px-4 py-6 '>
-    <Outlet />
-    </main>
-    <footer>
-      <Footer/>
-    </footer>
-    </div>
-    
-  )
+    <Provider store={appStore}>
+       <Toaster position="top-right" />
+      <div className="max-w-screen-xl mx-auto">
+        <Navbar />
+        <main className="min-h-screen mx-auto  px-4 py-6 ">
+          <Outlet />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
