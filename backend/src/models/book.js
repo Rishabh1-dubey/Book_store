6dfc0a1f928c752ae668cf8910a4324a83831bc8
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-09-22 13:17:38 +0530

    created a folder of book model router

---
+++ backend/src/models/book.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    trending: {
+      type: Boolean,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    oldPrice: Number,
+    newPrice: Number,
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model("Book", bookSchema)
+export default Book
