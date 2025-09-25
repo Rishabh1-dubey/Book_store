0c8889c43e899491c79a2bca62a8f386383f9e5f
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-09-25 14:09:00 +0530

    wrote  a logic of delete update and get

--- backend/src/controllers/book.controller.js
+++ backend/src/controllers/book.controller.js
@@ -46,8 +46,81 @@ export const createBook = async (req, res) => {
   }
 };
 
+//getAllBooks
 export const getAllBooks = async (req, res) => {
-  const allBook = await Book.find();
-  return res.status(200).json({ message: "All books fouund", success: true ,allBook});
+  try {
+    const allBook = await Book.find().sort({ created: -1 });
+    return res
+      .status(200)
+      .json({ message: "All books fouund", success: true, allBook });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error occured", success: false, error });
+  }
 };
 
+// get singleBook
+export const getSingleBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const singleBook = await Book.findById(id);
+    if (!singleBook) {
+      return res
+        .status(404)
+        .json({ message: "Not able to find the book", success: false });
+    }
+    return res
+      .status(200)
+      .json({ message: "All books fouund", success: true, singleBook });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error occured", success: false, error });
+  }
+};
+
+//update the book data
+export const updataBook = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+
+    //req.body kyu kiye hai see kyuki  tumko  sb field update krna haii iskeliye tum req.body kr rhe hoo
+    const updateBook = await Book.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updateBook) {
+      return res
+        .status(402)
+        .json({ message: "Not able to Update the book", success: false });
+    }
+    return res.status(200).json({
+      message: "update Books successfully",
+      success: true,
+      updateBook,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error occured", success: false, error });
+  }
+};
+
+// delete Book
+export const deleteBook = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleteBook = await Book.findByIdAndDelete(id);
+    if (!deleteBook) {
+      return res.status(402).json({
+        message: "Book not found ",
+        success: false,
+      });
+    }
+    return res
+      .status(200)
+      .json({ message: "Book deleted successfully", success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error occured", success: false, error });
+  }
+};
