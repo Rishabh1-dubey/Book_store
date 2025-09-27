7bb3c8d47c2687af1feb3ee9dac1121def1440b2
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-09-27 16:42:09 +0530

    created a auth contect for authentication

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,64 @@
+import { auth } from "@/config/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+const googleProvider = new GoogleAuthProvider();
+
+//authProvider
+export const AuthProvide = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  //registration a user
+  const registerUser = async (email, password) => {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // sing up with google
+  const signInWithGoogle = async () => {
+    return await signInWithPopup(auth, googleProvider);
+  };
+
+  // logout the user
+  const logout = () => {
+    return signOut(auth);
+  };
+
+   // manage user
+    useEffect(() => {
+        const unsubscribe =  onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            setLoading(false);
+
+            if(user) {
+               
+                const {email, displayName, photoURL} = user;
+                const userData = {
+                    email, username: displayName, photo: photoURL
+                } 
+            }
+        })
+
+        return () => unsubscribe();
+    }, [])
+
+   const value = {
+        currentUser,
+        loading,
+        registerUser,
+        signInWithGoogle,
+        logout
+    }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
