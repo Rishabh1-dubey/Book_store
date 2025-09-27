2a303b23b9bf911c3b18dbca10f767102aedfd19
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-09-27 13:41:56 +0530

    adding backend routes logic

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDb from "./src/database/connectDB.js";
 import bookRouter from "./src/routes/book.routes.js";
-
 
 dotenv.config();
 const port = process.env.PORT;
 const app = express();
 
-
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173"}));
 
 
 //database connection
-connectDb()
+connectDb();
 
 //routes-----------------------------------
 // Book routes --------------
-app.use(express.json())
-app.use("/api/v1/", bookRouter)
+app.use("/api/v1/", bookRouter);
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+
 app.listen(port, () => {
   console.log(`server is listening on ${port ? port : 3000}`);
 });
