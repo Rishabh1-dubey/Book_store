8a92280fa00ee6657367f71c535a522d0b901be0
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-10-01 17:29:59 +0530

    mostly frotend part done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,54 +1,15 @@
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import React from "react";
-import Home from "./pages/home/Home.jsx";
-import Login from "./pages/home/LogIN.jsx";
-import Register from "./pages/home/Register.jsx";
-import Cart from "./pages/home/Cart.jsx";
-import { CheckoutPage } from "./pages/home/CheckoutPage";
-import SingleBook from "./pages/home/singleBook";
 import { Provider } from "react-redux";
 import appStore from "./redux/store/appStore";
-import PrivateRoutes from "./routes/PrivateRoutes";
-
-
-
-const approuter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-       path:"/",
-       element:<Home/>
-      },
-      {
-        path:"/checkout",
-        element:<PrivateRoutes><CheckoutPage/></PrivateRoutes>
-      },{
-        path:"/dashboard",
-        element:<h2>the element is about</h2>
-      },{
-        path:"/login",
-        element:<Login/>
-      },{
-        path:"/cart",
-        element:<Cart/>
-      },{
-        path:"/books/:id",
-        element:<SingleBook/>
-      },
-    ]
-  },
-]);
+import router from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={appStore}>
-
-    <RouterProvider router={approuter} />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
