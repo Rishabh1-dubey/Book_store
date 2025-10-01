1cb6ddaede326e3ea7b3d73e134f2fd636295653
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-10-01 17:32:08 +0530

    adding user admin logic in our app

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./src/database/connectDB.js";
 import bookRouter from "./src/routes/book.routes.js";
+import orderRouter from "./src/routes/order.routes.js";
+import adminRouter from "./src/controllers/admin.controller.js";
 
 dotenv.config();
 const port = process.env.PORT;
@@ -18,8 +20,11 @@ connectDb();
 //routes-----------------------------------
 // Book routes --------------
 app.use("/api/v1/", bookRouter);
+app.use("/api/v1/order", orderRouter);
 
 
+//---------------- admin routes-------------------
+app.use("/api/", adminRouter)
 app.listen(port, () => {
   console.log(`server is listening on ${port ? port : 3000}`);
 });
