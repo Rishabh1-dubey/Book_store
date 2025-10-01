6f5c830e66c6f06162ad3bbe72f04b36be28f4a9
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-10-01 17:30:24 +0530

    added book routes

--- backend/src/routes/book.routes.js
+++ backend/src/routes/book.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createBook, deleteBook, getAllBooks, getSingleBook, updataBook } from "../controllers/book.controller.js";
+import verifyAdmin from "../middleware/verifyAdmin.js";
 
 
 const bookRouter = express.Router();
-bookRouter.post("/create-book", createBook);
+bookRouter.post("/create-book",verifyAdmin, createBook);
 bookRouter.get("/allbook", getAllBooks);
 bookRouter.get("/:id", getSingleBook);
-bookRouter.put("/edit/:id", updataBook);
-bookRouter.delete("/delete/:id", deleteBook);
+bookRouter.put("/edit/:id",verifyAdmin, updataBook);
+bookRouter.delete("/delete/:id",verifyAdmin, deleteBook);
 export default bookRouter;
